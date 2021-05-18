9b9d4450923d9dca23a08226a4fecdd98d52e7ab
Jan Fabio Nickels <fabio@Jans-MacBook-Pro.local>
2021-05-18 16:08:42 +0200

    Changes to be committed:

--- UtoS_profiles.py
+++ UtoS_profiles.py
@@ -179,172 +179,172 @@ for p in parameters:
             
             
             
-        # fitting function
-        def model(x,a,b):
-            return a*np.exp(-x/b)
+        # # fitting function
+        # def model(x,a,b):
+        #     return a*np.exp(-x/b)
         
-        time = np.array(range(duration))
+        # time = np.array(range(duration))
         
         
             
-        #Perform the curve fit
-        popt1, pcov1 = curve_fit(model, time[3:], wt_EcoRV_total_atf1_on[3:])
+        # #Perform the curve fit
+        # popt1, pcov1 = curve_fit(model, time[3:], wt_EcoRV_total_atf1_on[3:])
         
-        a1, b1 = unc.correlated_values(popt1, pcov1)
+        # a1, b1 = unc.correlated_values(popt1, pcov1)
         
-        # Plot data and best fit curve.
-        plt.scatter(time, wt_EcoRV_total_atf1_on, s=3, linewidth=0, alpha=0.3)
+        # # Plot data and best fit curve.
+        # plt.scatter(time, wt_EcoRV_total_atf1_on, s=3, linewidth=0, alpha=0.3)
         
-        px1 = np.linspace(1, 200, 200)
-        # use unumpy.exp
-        py1 = a1 * unp.exp(-px1/b1)
+        # px1 = np.linspace(1, 200, 200)
+        # # use unumpy.exp
+        # py1 = a1 * unp.exp(-px1/b1)
         
-        nom1 = unp.nominal_values(py1)
-        std1 = unp.std_devs(py1)
-        
-        
-        #fx values for the fitted function
-        xFit1 = np.arange(0.0, 200, 0.001)
+        # nom1 = unp.nominal_values(py1)
+        # std1 = unp.std_devs(py1)
         
         
-        
-        
-        
-        #Perform the curve fit
-        popt2, pcov2 = curve_fit(model, time[3:], wt_EcoRV_total_atf1_off[3:])
-        print(popt2)
-        
-        a2, b2 = unc.correlated_values(popt2, pcov2)
-        
-        # Plot data and best fit curve.
-        plt.scatter(time, wt_EcoRV_total_atf1_off, s=3, linewidth=0, alpha=0.3)
-        
-        px2 = np.linspace(1, 200, 200)
-        # use unumpy.exp
-        py2 = a2 * unp.exp(-px2/b2)
-        
-        nom2 = unp.nominal_values(py2)
-        std2 = unp.std_devs(py2)
-        
-        # plot the nominal value
-        plt.plot(px2, nom2, c='r')
-        
-        #fx values for the fitted function
-        xFit2 = np.arange(0.0, 200, 0.001)
+        # #fx values for the fitted function
+        # xFit1 = np.arange(0.0, 200, 0.001)
         
         
         
         
-        #Perform the curve fit
-        popt3, pcov3 = curve_fit(model, time[3:], wt_EcoRV_total_atf1_BS1_on[3:])
-        print(popt3)
         
-        a3, b3 = unc.correlated_values(popt3, pcov3)
+        # #Perform the curve fit
+        # popt2, pcov2 = curve_fit(model, time[3:], wt_EcoRV_total_atf1_off[3:])
+        # print(popt2)
         
-        # Plot data and best fit curve.
-        plt.scatter(time, wt_EcoRV_total_atf1_BS1_on, s=3, linewidth=0, alpha=0.3)
+        # a2, b2 = unc.correlated_values(popt2, pcov2)
         
-        px3 = np.linspace(1, 200, 200)
-        # use unumpy.exp
-        py3 = a3 * unp.exp(-px3/b3)
+        # # Plot data and best fit curve.
+        # plt.scatter(time, wt_EcoRV_total_atf1_off, s=3, linewidth=0, alpha=0.3)
         
-        nom3 = unp.nominal_values(py3)
-        std3 = unp.std_devs(py3)
+        # px2 = np.linspace(1, 200, 200)
+        # # use unumpy.exp
+        # py2 = a2 * unp.exp(-px2/b2)
         
-        # plot the nominal value
-        plt.plot(px3, nom3, c='r')
+        # nom2 = unp.nominal_values(py2)
+        # std2 = unp.std_devs(py2)
         
-        #fx values for the fitted function
-        xFit3 = np.arange(0.0, 200, 0.001)
+        # # plot the nominal value
+        # plt.plot(px2, nom2, c='r')
         
-        
-        
-        
-        #Perform the curve fit
-        popt4, pcov4 = curve_fit(model, time[3:], wt_EcoRV_total_atf1_BS2_on[3:])
-        print(popt4)
-        
-        a4, b4 = unc.correlated_values(popt4, pcov4)
-        
-        # Plot data and best fit curve.
-        plt.scatter(time, wt_EcoRV_total_atf1_BS1_on, s=3, linewidth=0, alpha=0.3)
-        
-        px4 = np.linspace(1, 200, 200)
-        # use unumpy.exp
-        py4 = a4 * unp.exp(-px4/b4)
-        
-        nom4 = unp.nominal_values(py4)
-        std4 = unp.std_devs(py4)
-        
-        # plot the nominal value
-        plt.plot(px4, nom4, c='r')
-        
-        #fx values for the fitted function
-        xFit4 = np.arange(0.0, 200, 0.001)
+        # #fx values for the fitted function
+        # xFit2 = np.arange(0.0, 200, 0.001)
         
         
         
         
-        #Perform the curve fit
-        popt5, pcov5 = curve_fit(model, time[3:], wt_cenH_all_data_points[3:])
-        print(popt5)
+        # #Perform the curve fit
+        # popt3, pcov3 = curve_fit(model, time[3:], wt_EcoRV_total_atf1_BS1_on[3:])
+        # print(popt3)
         
-        a5, b5 = unc.correlated_values(popt5, pcov5)
+        # a3, b3 = unc.correlated_values(popt3, pcov3)
         
-        # Plot data and best fit curve.
-        plt.scatter(time, wt_cenH_all_data_points, s=3, linewidth=0, alpha=0.3)
+        # # Plot data and best fit curve.
+        # plt.scatter(time, wt_EcoRV_total_atf1_BS1_on, s=3, linewidth=0, alpha=0.3)
         
-        px5 = np.linspace(1, 200, 200)
-        # use unumpy.exp
-        py5 = a5 * unp.exp(-px5/b5)
+        # px3 = np.linspace(1, 200, 200)
+        # # use unumpy.exp
+        # py3 = a3 * unp.exp(-px3/b3)
         
-        nom5 = unp.nominal_values(py5)
-        std5 = unp.std_devs(py5)
+        # nom3 = unp.nominal_values(py3)
+        # std3 = unp.std_devs(py3)
         
-        # plot the nominal value
-        plt.plot(px5, nom5, c='r')
+        # # plot the nominal value
+        # plt.plot(px3, nom3, c='r')
         
-        #fx values for the fitted function
-        xFit5 = np.arange(0.0, 200, 0.001)
+        # #fx values for the fitted function
+        # xFit3 = np.arange(0.0, 200, 0.001)
+        
+        
+        
+        
+        # #Perform the curve fit
+        # popt4, pcov4 = curve_fit(model, time[3:], wt_EcoRV_total_atf1_BS2_on[3:])
+        # print(popt4)
+        
+        # a4, b4 = unc.correlated_values(popt4, pcov4)
+        
+        # # Plot data and best fit curve.
+        # plt.scatter(time, wt_EcoRV_total_atf1_BS1_on, s=3, linewidth=0, alpha=0.3)
+        
+        # px4 = np.linspace(1, 200, 200)
+        # # use unumpy.exp
+        # py4 = a4 * unp.exp(-px4/b4)
+        
+        # nom4 = unp.nominal_values(py4)
+        # std4 = unp.std_devs(py4)
+        
+        # # plot the nominal value
+        # plt.plot(px4, nom4, c='r')
+        
+        # #fx values for the fitted function
+        # xFit4 = np.arange(0.0, 200, 0.001)
+        
+        
+        
+        
+        # #Perform the curve fit
+        # popt5, pcov5 = curve_fit(model, time[3:], wt_cenH_all_data_points[3:])
+        # print(popt5)
+        
+        # a5, b5 = unc.correlated_values(popt5, pcov5)
+        
+        # # Plot data and best fit curve.
+        # plt.scatter(time, wt_cenH_all_data_points, s=3, linewidth=0, alpha=0.3)
+        
+        # px5 = np.linspace(1, 200, 200)
+        # # use unumpy.exp
+        # py5 = a5 * unp.exp(-px5/b5)
+        
+        # nom5 = unp.nominal_values(py5)
+        # std5 = unp.std_devs(py5)
+        
+        # # plot the nominal value
+        # plt.plot(px5, nom5, c='r')
+        
+        # #fx values for the fitted function
+        # xFit5 = np.arange(0.0, 200, 0.001)
         
 
         
-        #plot the fitted function
-        plt.figure(figsize=[15,10])
-        #plt.plot(xFit1, model(xFit1, *popt1), label='fit params: a=%5.1f, b=%5.1f' % tuple(popt1), lw=3.)
-        plt.plot(time, wt_EcoRV_total_atf1_on, 'b', label='model data (atf1 on) with a=%5.1f, b=%5.1f' % tuple(popt1), lw=3.)
+        # #plot the fitted function
+        # plt.figure(figsize=[15,10])
+        # #plt.plot(xFit1, model(xFit1, *popt1), label='fit params: a=%5.1f, b=%5.1f' % tuple(popt1), lw=3.)
+        # plt.plot(time, wt_EcoRV_total_atf1_on, 'b', label='model data (atf1 on) with a=%5.1f, b=%5.1f' % tuple(popt1), lw=3.)
         
         
-        #plt.plot(xFit2, model(xFit2, *popt2), 'r', label='fit params: a=%5.1f, b=%5.1f' % tuple(popt2), lw=3.)
-        plt.plot(time, wt_EcoRV_total_atf1_off, 'r', label='model data (atf1 off) with a=%5.1f, b=%5.1f' % tuple(popt2), lw=3.)
-        # 
+        # #plt.plot(xFit2, model(xFit2, *popt2), 'r', label='fit params: a=%5.1f, b=%5.1f' % tuple(popt2), lw=3.)
+        # plt.plot(time, wt_EcoRV_total_atf1_off, 'r', label='model data (atf1 off) with a=%5.1f, b=%5.1f' % tuple(popt2), lw=3.)
+        # # 
         
         
-        #plt.plot(xFit3, model(xFit3, *popt3), 'g', label='fit params: a=%5.1f, b=%5.1f' % tuple(popt3), lw=3.)
-        plt.plot(time, wt_EcoRV_total_atf1_BS1_on, 'g', label='model data (only BS1 on) witha=%5.1f, b=%5.1f' % tuple(popt3), lw=3.)
+        # #plt.plot(xFit3, model(xFit3, *popt3), 'g', label='fit params: a=%5.1f, b=%5.1f' % tuple(popt3), lw=3.)
+        # plt.plot(time, wt_EcoRV_total_atf1_BS1_on, 'g', label='model data (only BS1 on) witha=%5.1f, b=%5.1f' % tuple(popt3), lw=3.)
         
         
-        #plt.plot(xFit3, model(xFit3, *popt3), 'g', label='fit params: a=%5.1f, b=%5.1f' % tuple(popt3), lw=3.)
-        plt.plot(time, wt_EcoRV_total_atf1_BS1_on, 'salmon', label='model data (only BS2 on) with a=%5.1f, b=%5.1f' % tuple(popt4), lw=3.)
+        # #plt.plot(xFit3, model(xFit3, *popt3), 'g', label='fit params: a=%5.1f, b=%5.1f' % tuple(popt3), lw=3.)
+        # plt.plot(time, wt_EcoRV_total_atf1_BS1_on, 'salmon', label='model data (only BS2 on) with a=%5.1f, b=%5.1f' % tuple(popt4), lw=3.)
         
-        #plt.plot(xFit3, model(xFit3, *popt3), 'g', label='fit params: a=%5.1f, b=%5.1f' % tuple(popt3), lw=3.)
-        plt.plot(time, wt_cenH_all_data_points, 'yellow', label='model data (cenH all systems) ', lw=2.)
-        #plt.plot(time, cenH2, 'salmon', label='model data (cenH: atf1 off) with a=%5.1f, b=%5.1f' % tuple(popt5), lw=3.)
-        #plt.plot(time, cenH3, 'lightgreen', label='model data (cenH: ME20)', lw=2.)
+        # #plt.plot(xFit3, model(xFit3, *popt3), 'g', label='fit params: a=%5.1f, b=%5.1f' % tuple(popt3), lw=3.)
+        # plt.plot(time, wt_cenH_all_data_points, 'yellow', label='model data (cenH all systems) ', lw=2.)
+        # #plt.plot(time, cenH2, 'salmon', label='model data (cenH: atf1 off) with a=%5.1f, b=%5.1f' % tuple(popt5), lw=3.)
+        # #plt.plot(time, cenH3, 'lightgreen', label='model data (cenH: ME20)', lw=2.)
         
-        plt.ylim([0.001,1])
-        plt.yticks(fontsize=40)
-        plt.xticks(fontsize=30)
-        plt.xlim([0,200])
-        plt.yscale('log')
-        plt.xticks([25, 50, 75, 100, 125, 150, 175, 200])
-        plt.yticks([1,0.1,0.01],[1, 0.1, 0.01])
-        #plt.ylabel('fraction of "ON" cells', fontsize=30)
-        #plt.xlabel('time (generations)', fontsize=30)
-        plt.legend(fontsize=18,loc="upper right")
-        plt.tick_params(width=4,length=4)
+        # plt.ylim([0.001,1])
+        # plt.yticks(fontsize=40)
+        # plt.xticks(fontsize=30)
+        # plt.xlim([0,200])
+        # plt.yscale('log')
+        # plt.xticks([25, 50, 75, 100, 125, 150, 175, 200])
+        # plt.yticks([1,0.1,0.01],[1, 0.1, 0.01])
+        # #plt.ylabel('fraction of "ON" cells', fontsize=30)
+        # #plt.xlabel('time (generations)', fontsize=30)
+        # plt.legend(fontsize=18,loc="upper right")
+        # plt.tick_params(width=4,length=4)
         
-        plt.savefig('wt_UtoS_model_atf1_UtoS_%_AtoU_%_direct_%.txt' %tuple(p), format='pdf')
+        # plt.savefig('wt_UtoS_model_atf1_UtoS_%_AtoU_%_direct_%.txt' %tuple(p), format='pdf')
         
     
 
