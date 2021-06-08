4a9a0a8f198f7ba84611b261057927b77a0a3211
Jan Fabio Nickels <fabio@Jans-MacBook-Pro.local>
2021-06-08 14:35:23 +0200

    Changes to be committed:

--- AtoU_profile_dcr_del.py
+++ AtoU_profile_dcr_del.py
@@ -94,12 +94,12 @@ for p in parameters:
         
         
         # save state_list
-        with open('gAtoU_dcr_del_cenH_Atf1_60_S130_UtoS%5.1f_AtoU%5.1f_direct%5.1f.txt' %tuple(p), 'wb') as F:
+        with open('gAtoU_dcr_del_reswitch_cenH_Atf1_60_S130_UtoS%5.1f_AtoU%5.1f_direct%5.1f.txt' %tuple(p), 'wb') as F:
             pickle.dump(cenH_total, F)
             
         
         # save state_list
-        with open('gAtoU_dcr_del_EcoRV_Atf1_60_S130_UtoS%5.1f_AtoU%5.1f_direct%5.1f.txt' %tuple(p), 'wb') as F:
+        with open('gAtoU_dcr_del_reswitch_EcoRV_Atf1_60_S130_UtoS%5.1f_AtoU%5.1f_direct%5.1f.txt' %tuple(p), 'wb') as F:
             pickle.dump(EcoRV_total, F)
 
 
